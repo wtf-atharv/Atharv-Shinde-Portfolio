82e4ddb28940e2592c2a5f865c8a0365194bd222
wtf-atharv <151851056+wtf-atharv@users.noreply.github.com>
2026-01-10 14:17:39 +0530

    Change base path for deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 this fixes the blank page on deployment
+  base: '/Atharv-Shinde-Portfolio', // 👈 this fixes the blank page on deployment
 })
